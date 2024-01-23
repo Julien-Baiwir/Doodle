5863debcdb7612a1c3f0dae88a0b6fc9c531e83b
Julien-Baiwir <julien.freddy.baiwir@gmail.com>
2024-01-23 14:30:12 +0100

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,6 @@
-
+// Pour obtenir tous les événements
+fetch('http://localhost:3000/api/events')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error));
   
