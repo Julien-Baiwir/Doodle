41932a9c8848b5ea3b871f1c6bc18cc0a9476407
Julien-Baiwir <julien.freddy.baiwir@gmail.com>
2024-01-23 14:18:12 +0100

    premiertest

---
+++ assets/js/testFunction.js
@@ -0,0 +1,36 @@
+async function fetchData() {
+    try {
+      const response = await fetch('http://localhost:3000/api/events');
+      const data = await response.json();
+  
+      const eventListContainer = document.getElementById('eventList');
+  
+      data.forEach(event => {
+        const eventDiv = document.createElement('div');
+        eventDiv.innerHTML = `<h3>${event.name}</h3><p>${event.description}</p>`;
+        eventListContainer.appendChild(eventDiv);
+      });
+    } catch (error) {
+      console.error('Erreur lors de la récupération des données:', error);
+    }
+  }
+
+
+fetchData();
+  
+  async function fetchSpecificEvent(eventId) {
+    try {
+      const response = await fetch(`http://localhost:3000/api/events/${eventId}`);
+      const eventData = await response.json();
+  
+      
+      const eventContainer = document.getElementById('specificEvent');
+      const eventDiv = document.createElement('div');
+      eventDiv.innerHTML = `<h3>${eventData.name}</h3><p>${eventData.description}</p>`;
+      eventContainer.appendChild(eventDiv);
+    } catch (error) {
+      console.error('Erreur lors de la récupération des données de l\'événement:', error);
+    }
+  }
+  
+  fetchSpecificEvent('f5b6564b5dc4');
