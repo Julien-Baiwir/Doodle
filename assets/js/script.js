17f88ac95670a75cbde1220a5624a6a8794cd9b9
Julien-Baiwir <julien.freddy.baiwir@gmail.com>
2024-01-24 11:04:50 +0100

    POSTevent

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,47 @@
 import { initializeUsername } from './username.js';
-
 initializeUsername();
+
+const createEventForm = document.getElementById('create-event');
+
+createEventForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const authorInput = document.getElementById('event-author');
+  const nameInput = document.getElementById('event-name');
+  const descriptionInput = document.getElementById('event-description');
+  const dateInput = document.getElementById('event-date');
+
+  const eventData = {
+    author: authorInput.value,
+    name: nameInput.value,
+    description: descriptionInput.value,
+    dates: [dateInput.value],
+  };
+
+  try {
+    const response = await fetch('http://localhost:3000/api/events', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(eventData),
+    });
+
+    if (response.ok) {
+      const newEvent = await response.json();
+      console.log('Event created:', newEvent);
+
+    } else {
+      const errorData = await response.json();
+       console.error('Error creating event:', errorData);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+});
+
+
+
+
+
 
--- assets/js/username.js
+++ assets/js/username.js
@@ -1,25 +1,16 @@
 export function initializeUsername() {
-    const newEvent = JSON.parse(localStorage.getItem('newEvent')) || {};
-
-    if (!newEvent.author) {
+    const usernameElement = document.getElementById('username');
+    
+    if (!usernameElement.textContent) {
         const enteredUsername = prompt("Please enter your username:");
+        const authorInputElement = document.getElementById('event-author');
+authorInputElement.value = enteredUsername;
 
         if (enteredUsername) {
-            newEvent.author = enteredUsername;
-            localStorage.setItem('newEvent', JSON.stringify(newEvent));
-
-            const usernameElement = document.getElementById('username');
             usernameElement.textContent = `Welcome, ${enteredUsername}`;
             usernameElement.classList.add('user-welcome');
         } else {
             alert("You must enter a username. Please refresh the page and try again.");
         }
-    } else {
-        const usernameElement = document.getElementById('username');
-        usernameElement.textContent = `Welcome, ${newEvent.author}`;
-        usernameElement.classList.add('user-welcome');
     }
 }
-
-
-
